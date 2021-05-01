0afcd44b4effa7ac26b3a5e463d969a2d0ecb091
Christian Bernhard <christian.bernhard@adesso.de>
2021-05-01 18:06:48 +0200

    Added Logic

--- devops/devops/ViewController.swift
+++ devops/devops/ViewController.swift
@@ -11,13 +11,24 @@ class ViewController: UIViewController {
     @IBOutlet var myView: UIView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var firstNumberLabel: UILabel!
+    @IBOutlet weak var firstNumberInput: UITextField!
+    @IBOutlet weak var secondNumberLabel: UILabel!
+    @IBOutlet weak var secondNumberInput: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var output: UILabel!
+    @IBOutlet weak var calculateButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupGradient()
-        setupLabels()
+        setupHeadlineLabel()
+        setupNumberLabels()
+        setupOutputLabels()
+        setupNumberInputs()
+        setupButton()
     }
     
     func setupGradient() {
@@ -32,10 +43,55 @@ class ViewController: UIViewController {
         self.view.layer.insertSublayer(gradient, at: 0)
     }
     
-    func setupLabels() {
+    func setupHeadlineLabel() {
         headlineLabel.text = "Multiplicator"
         headlineLabel.textColor = .black
         headlineLabel.font = UIFont.systemFont(ofSize: 30.0, weight: .light)
+    }
+    
+    func setupNumberLabels() {
+        firstNumberLabel.text = "First Number"
+        firstNumberLabel.textColor = .black
+        firstNumberLabel.font = UIFont.systemFont(ofSize: 15.0, weight: .light)
+        
+        secondNumberLabel.text = "Second Number"
+        secondNumberLabel.textColor = .black
+        secondNumberLabel.font = UIFont.systemFont(ofSize: 15.0, weight: .light)
+    }
+    
+    func setupNumberInputs() {
+        firstNumberInput.frame.size = CGSize(width: 200, height: 30)
+    }
+    
+    func setupOutputLabels() {
+        outputLabel.text = "Result:"
+        outputLabel.textColor = .black
+        outputLabel.font = UIFont.systemFont(ofSize: 15.0, weight: .light)
+        
+        output.text = "-"
+        output.textColor = .black
+        output.font = UIFont.systemFont(ofSize: 15.0, weight: .light)
+    }
+    
+    func setupButton() {
+        calculateButton.backgroundColor = .lightGray
+        calculateButton.setTitle("Calculate", for: .normal)
+        calculateButton.titleLabel?.font = UIFont.systemFont(ofSize: 30, weight: .light)
+        calculateButton.setTitleColor(.black, for: .normal)
+        //calculateButton.frame.size = CGSize(width: 200, height: 50)
+        calculateButton.layer.cornerRadius = CGFloat(18.0)
+    }
+    
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        output.text = calculateMultiplication()
+    }
+    
+    func calculateMultiplication() -> String {
+        let a = Int(firstNumberInput.text ?? "0") ?? 0
+        let b = Int(secondNumberInput.text ?? "0") ?? 0
+        let result  = String(a*b)
+        return result
     }
     
 }
